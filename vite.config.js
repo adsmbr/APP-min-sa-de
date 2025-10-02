95acba007b79ce82716afe92d62454049bb2e56c
Seu Nome <seu-email@exemplo.com>
2025-10-02 10:58:02 -0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/APP-min-sa-de/",
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
-  }
-})
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
+});
